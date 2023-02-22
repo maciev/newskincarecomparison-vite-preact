b2e3c2d271977ba8a0be65048903458fbf03d2b7
Andrew Chatelain <maciev@Andrews-Laptop.local>
2023-02-22 18:54:16 -0300

    working styled components and early routing

--- pages/Homescreen/Homescreen.tsx
+++ pages/Homescreen/Homescreen.tsx
@@ -1,9 +1,43 @@
 import { render } from "preact";
+//import { Link } from "preact-router";
+import { useState } from "preact/hooks";
+import preactLogo from "../../src/assets/preact.svg";
+import styled from "styled-components";
+import Link from "preact-router/match";
 
 export function Homescreen() {
+  const [count, setCount] = useState(0);
+
+  const Highlight = styled.h1`
+    background-color: red;
+  `;
+
   return (
     <>
-      <h1>About</h1>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://preactjs.com" target="_blank">
+          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+        </a>
+      </div>
+      <Highlight>Vite + Preact</Highlight>
+      <div class="card">
+        <button
+          onClick={() => {
+            <Link path="/product" />;
+          }}
+        >
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/app.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and Preact logos to learn more
+      </p>
     </>
   );
 }
---
+++ pages/Productpage/Productpage.tsx
@@ -0,0 +1,11 @@
+import { render } from "preact";
+
+export function Productpage() {
+  return (
+    <>
+      <div>test</div>
+    </>
+  );
+}
+
+render(<Productpage />, document.getElementById("app")!);
